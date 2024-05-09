ab9c925f36cbfa509296b5f29395cb4f016c5050
Aya E Mahmoud <ayae.mahmoud@gmail.com>
2024-05-09 10:41:20 +0300

    Add Floating Point test case

---
+++ Sources/MyLibrary/FloatingPoint.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aya Mahmoud on 09/05/2024.
+//
+extension FloatingPoint {
+    var isInteger: Bool { rounded() == self }
+}
---
+++ Tests/MyLibraryTests/FloatingPointTestCase.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aya Mahmoud on 09/05/2024.
+//
+
+import Foundation
+import XCTest
+@testable import MyLibrary
+
+class FloatingPointTestCase: XCTestCase {
+    
+    func text_valid() {
+        
+    }
+    
+    func test_invalid() {
+        
+    }
+}
